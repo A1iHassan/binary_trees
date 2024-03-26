ee070a7e8e25947a818921abfb09d7b3992453d3
A1iHassan <ali012wkout@gmail.com>
2024-03-27 01:34:34 +0200

    commit by Ali Hassan

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,23 +11,25 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = binary_tree_node(parent, value), *temp = NULL;
 
-	if (parent == NULL)
-		return (NULL);
-	node = binary_tree_node(parent, value);
-	if (node == NULL)
+	if (!node || !parent)
 	{
 		free(node);
 		return (NULL);
 	}
 	node->parent = parent;
 	node->n = value;
-	if (parent->left != NULL)
+	if (parent->left == NULL)
 	{
-		node->left = parent->left;
-		parent->left->parent = node;
+		parent->left = node;
 	}
-	parent->left = node;
+	else
+	{
+		temp = parent->left;
+		parent->left = node;
+		node->left = temp;
+		temp->parent = node;
+	}
 	return (node);
 }
