24119e29cdece33958819fbb894b0836a899f023
A1iHassan <ali012wkout@gmail.com>
2024-03-27 01:33:32 +0200

    commit by Ali Hassan

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,7 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t)), *temp = NULL;
+	binary_tree_t *node = binary_tree_node(parent, value), *temp = NULL;
 
 	if (!node || !parent)
 	{
