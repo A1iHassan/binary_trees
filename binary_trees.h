35dba8a988606081656a851e2dc576906cd25ee8
A1iHassan <ali012wkout@gmail.com>
2024-03-27 01:19:35 +0200

    commit by Ali Hassan

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
 
 /**
@@ -14,13 +14,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
 typedef struct binary_tree_s binary_tree_t;
 
@@ -51,4 +51,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /*BINARY_TREES_H*/
